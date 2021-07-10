4d51977237e9893a729780de77efc3db5b18187c
mstislav <strong13strong@gmail.com>
2021-07-11 00:39:57 +0700

    add lesson 6

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from basketapp.models import Basket
 from mainapp.models import Product
 
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def basket(request):
     if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+        basket = Basket.objects.filter(user=request.user).order_by('product__category')
         context = {
             'basket': basket
         }
@@ -15,7 +18,10 @@ def basket(request):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+@login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
 
@@ -27,5 +33,8 @@ def basket_add(request, pk):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-def basket_remove(request):
-    return render(request, 'basketapp/basket.html')
+@login_required
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
