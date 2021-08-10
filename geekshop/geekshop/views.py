82c61f5086c31f7ed1d314156de9e7d1687a18e7
mstislav <strong13strong@gmail.com>
2021-08-10 21:39:18 +0700

    add django2_lesson_6

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -7,7 +7,7 @@ from mainapp.models import Product
 def index(request):
     title = 'магазин'
 
-    products = Product.objects.all()[:5]
+    products = Product.objects.filter(is_deleted=False, category__is_deleted=False)[:5]
 
     context = {
         'title': title,
@@ -18,11 +18,11 @@ def index(request):
 def contacts(request):
     title = 'контакты'
 
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+    # if request.user.is_authenticated:
+    #     basket = Basket.objects.filter(user=request.user)
 
     context = {
         'title': title,
-        'basket': basket,
+        # 'basket': basket,
     }
     return render(request, 'geekshop/contact.html', context=context)
